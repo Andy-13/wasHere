a7c1b9e99e78923b4013e6336d2aad0f9fa8497e
Aria <574335078@qq.com>
2017-09-02 16:10:33 +0800

    finish findmessage

---
+++ app/src/main/java/com/example/yang/washere/FindMsssage/NetworkUtils.java
@@ -0,0 +1,68 @@
+package com.example.yang.washere.FindMsssage;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by Aria on 2017/9/2.
+ */
+
+public class NetworkUtils {
+
+    private static final String TAG = "NetworkUtils";
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private static OkHttpClient okHttpClient;
+
+    private static void init(){
+        okHttpClient = new OkHttpClient.Builder().writeTimeout(2000, TimeUnit.MILLISECONDS)
+                            .readTimeout(2000,TimeUnit.MILLISECONDS)
+                            .build();
+    }
+
+    public static Call doPost(String url, JSONObject object) throws IOException {
+        Log.d("MainActivity","object:"+object.toString());
+        RequestBody body = RequestBody.create(JSON,object.toString());
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        if (okHttpClient == null)
+            init();
+        return okHttpClient.newCall(request);
+
+    }
+
+    public static Call doPost(String url, String object) throws IOException {
+        Log.d(TAG,"object:"+object);
+        RequestBody body = RequestBody.create(JSON,object);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        if (okHttpClient == null)
+            init();
+        return okHttpClient.newCall(request);
+    }
+
+    public static Call doGet(String url){
+        if (okHttpClient == null)
+            init();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        return okHttpClient.newCall(request);
+    }
+}
